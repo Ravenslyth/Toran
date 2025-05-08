61200ae9afc23ab17399c3de92ece892d3a3c4f8
odistudiogames <odistudio4444@gmail.com>
2025-05-08 20:08:47 +0100

    Ennemie back to origine position

--- script/ennemies/mob1.gd
+++ script/ennemies/mob1.gd
@@ -6,26 +6,38 @@ class_name ennemy
 
 @export var target : Node2D
 
-#
+#----------------------Statut of move----------------------------------------------
 
 var see_player : bool = false # use when the ennemie see the player
 var last_move : bool = false # use when the ennemie finish the navigation 
+var move_to_origine_position : bool = false
 
-#
+#-----------------Navigation agent 2d---------------(
 
 @onready var navigation_agent_2d = $NavigationAgent2D
+
+#----------------Timer preload------------------------
+
 @onready var out_of_view = $OutOfView
+@onready var back_to_origine_position = $BackToOriginePosition
+
+#----------------Label preload-------------------
 
 @onready var see_you = $See_you
 @onready var not_see_you = $Not_See_you
 
-#
+#--------------------Origine position------------------
+
+var origine_position = Vector2.ZERO
+
+#------------------Speed------------------------------
 
 @export var speed = 100
 
+#----------------------------------------------------
 
 func _ready():
-	pass
+	origine_position = global_position
 
 func _process(delta):
 	#------------------------Move at the player-----------------------
@@ -33,7 +45,11 @@ func _process(delta):
 		var direction = to_local(navigation_agent_2d.get_next_path_position())
 		direction = direction.normalized()
 		velocity = direction * speed
-	else:
+	elif move_to_origine_position and last_move:
+		var direction = to_local(navigation_agent_2d.get_next_path_position())
+		direction = direction.normalized()
+		velocity = direction * speed
+	elif not move_to_origine_position:
 		velocity = Vector2.ZERO
 	move_and_slide()
 	#------------------------End Move at the player-----------------------
@@ -60,16 +76,20 @@ func _on_zone_of_detection_body_entered(body):
 func _on_zone_of_detection_body_exited(body):
 	out_of_view.start()
 	see_player = false
+	move_to_origine_position = true
 	see_you.hide()
 	not_see_you.show()
 	print("exited")
 
 
 func _on_out_of_view_timeout():
+	navigation_agent_2d.target_position = origine_position
+	move_to_origine_position = true
 	not_see_you.hide()
 
 func _on_navigation_agent_2d_navigation_finished():
 	velocity = Vector2.ZERO
 	last_move = true
+	move_to_origine_position = false
 	print("stop")
 #------------------End Player Out Detection--------------------
